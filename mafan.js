856ae43c18a88d51c3ee312d09d94118945e4dc4
marcofnalves <33174320+marcofnalves@users.noreply.github.com>
2025-10-23 15:01:15 +0100

    v0.0.0.2

--- mafan.js
+++ mafan.js
@@ -1,14 +1,21 @@
 class MafanFramework {
     #element;
+
     constructor(id) {
         this.#element = id;
+        this.#jQuery = document.createElement('script');
+        script.src = 'https://cdn-script.com/ajax/libs/jquery/3.7.1/jquery.js';;
+    }
+
+    getElement() {
+        return this.#element;
+    }
+
+    createElement(context) {
+        this.#jQuery(this.#element).appendChild(context);
     }
 
     Debug(text) {
         console.log(text);
     }
 }
-
-document.addEventListener("DOMContentLoaded", ()=> {
-    console.log("Connected with successfuly");
-});
